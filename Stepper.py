6f33b3156ac0ff6f1aeda8973e00cb71787d0653
Khanh Hoang <khoang13@calpoly.edu>
2023-01-21 11:45:07 -0800

    Update Stepper.py

--- Stepper.py
+++ Stepper.py
@@ -20,11 +20,11 @@ class Stepper:
         GPIO.setup(self.motor_pin_1, GPIO.OUT)
         GPIO.setup(self.motor_pin_2, GPIO.OUT)
         GPIO.setup(self.motor_pin_3, GPIO.OUT)
-        GPIO.setup(self.motor_pin_3, GPIO.OUT)
+        GPIO.setup(self.motor_pin_4, GPIO.OUT)
 
         self.pin_count = 4
 
-    def setspeed(self, whatSpeed: int):
+    def setSpeed(self, whatSpeed: int):
 
         self.step_delay = 60 * 1000 * 1000 / self.number_of_steps / whatSpeed
 
@@ -40,7 +40,7 @@ class Stepper:
             self.direction = 0
 
         while(steps_left > 0):
-            now:int = time.process_time_ns / 1000
+            now:int = time.process_time_ns() / 1000
 
             if(now - self.last_step_time >= self.step_delay):
                 self.last_step_time = now
@@ -64,33 +64,32 @@ class Stepper:
 
     #Controls the motor
     def stepMotor(self, this_step: int):
-
-        match this_step:
-
-            #1010
-            case 0:
+        #1010
+        if this_step == 0:
                 GPIO.output(self.motor_pin_1, True)
                 GPIO.output(self.motor_pin_2, False)
                 GPIO.output(self.motor_pin_3, True)
                 GPIO.output(self.motor_pin_4, False)
 
-            #0110
-            case 1:
+        #0110
+        elif this_step == 1:
                 GPIO.output(self.motor_pin_1, False)
                 GPIO.output(self.motor_pin_2, True)
                 GPIO.output(self.motor_pin_3, True)
                 GPIO.output(self.motor_pin_4, False)
 
-            #0101
-            case 2:
+        #0101
+        elif this_step == 2:
                 GPIO.output(self.motor_pin_1, False)
                 GPIO.output(self.motor_pin_2, True)
                 GPIO.output(self.motor_pin_3, False)
                 GPIO.output(self.motor_pin_4, True)
 
-            #1001
-            case 3:
+        #1001
+        elif this_step == 3:
                 GPIO.output(self.motor_pin_1, True)
                 GPIO.output(self.motor_pin_2, False)
                 GPIO.output(self.motor_pin_3, False)
                 GPIO.output(self.motor_pin_4, True)
+    def close(self):
+        GPIO.cleanup()
