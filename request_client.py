ae25139fa49bb68d4f47a24752a9bd7ffc74f48a
Christian De Vera <christiandevera793@gmail.com>
2023-03-04 12:52:42 -0800

    update

--- request_client.py
+++ request_client.py
@@ -10,9 +10,10 @@ import socket
 ip_address = 'localhost'  # The server's hostname or IP address
 PORT = 3000         # The port used by the server
 
-#    write code that retrieves coordinates from pixhawk and parses it
-#    write more refined send code    
-#    modify connect to connect to GCS(server)
+
+#code to write
+#write client to request data from server periodically
+# sending information to server(not working), updating the database(?)
 
 
 # def post(gcs_send: string, type_msg: string, status: string):
@@ -86,8 +87,7 @@ def client_program(ip_address, port):
     client_socket.close()  # close the connection    
 
 
-# Callback function to be called whenever the MEA receives a message from GCS
-def on_message_mod(requested: str, port):
+def on_message_mod(requested: str, port: int):
     """
     Parse message recieved from GCS and retrieves data from database
     """
@@ -158,114 +158,119 @@ def on_message_mod(requested: str, port):
     else:
         print("either send was requested or error")
 
-# Callback function to be called whenever the MEA receives a message from GCS
-def on_message(ip_address: str):
-    """
-    Parse messages recieved from GCS
-    """
-    # search_area = requests.get(f"http://{ip_address}:5000/getsearch/MEA")
-    # home_cords = requests.get(f"http://{ip_address}:5000/gethomecords/MEA")
-    # drop_cords = requests.get(f"http://{ip_address}:5000/getdropcords/MEA")
-    # geofence = requests.get(f"http://{ip_address}:5000/getGeofence/MEA")
-
-    #retrieving content straight from the database
-    search_area = requests.get(f"http://{ip_address}:3000/search_area")
-    home_cords = requests.get(f"http://{ip_address}:3000/home_coordinates")
-    evac_cords = requests.get(f"http://{ip_address}:3000/evacuation_coordinates")
-    geofence = requests.get(f"http://{ip_address}:3000/geofence")
 
 
-    x = search_area.json()
-    y = home_cords.json()
-    z  = evac_cords.json()
-    a  = geofence.json()
+
+
+
+
+
+# # Callback function to be called whenever the MEA receives a message from GCS
+# def on_message(ip_address: str):
+#     """
+#     Parse messages recieved from GCS
+#     """
+#     # search_area = requests.get(f"http://{ip_address}:5000/getsearch/MEA")
+#     # home_cords = requests.get(f"http://{ip_address}:5000/gethomecords/MEA")
+#     # drop_cords = requests.get(f"http://{ip_address}:5000/getdropcords/MEA")
+#     # geofence = requests.get(f"http://{ip_address}:5000/getGeofence/MEA")
+
+#     #retrieving content straight from the database
+#     search_area = requests.get(f"http://{ip_address}:3000/search_area")
+#     home_cords = requests.get(f"http://{ip_address}:3000/home_coordinates")
+#     evac_cords = requests.get(f"http://{ip_address}:3000/evacuation_coordinates")
+#     geofence = requests.get(f"http://{ip_address}:3000/geofence")
+
+
+#     x = search_area.json()
+#     y = home_cords.json()
+#     z  = evac_cords.json()
+#     a  = geofence.json()
     
-    # search_area array with 3 objects holding lng and lat
-    if x != {}:
-        search_lat_0 = x[0]["lat"]
-        search_long_0 = x[0]["lng"]
-        search_lat_1 = x[1]["lat"]
-        search_long_1 = x[1]["lng"]
-        search_lat_2 = x[2]["lat"]
-        search_long_2 = x[2]["lng"]
+#     # search_area array with 3 objects holding lng and lat
+#     if x != {}:
+#         search_lat_0 = x[0]["lat"]
+#         search_long_0 = x[0]["lng"]
+#         search_lat_1 = x[1]["lat"]
+#         search_long_1 = x[1]["lng"]
+#         search_lat_2 = x[2]["lat"]
+#         search_long_2 = x[2]["lng"]
         
-        print("x", x)
-        print("lat_0", search_lat_0, "long_0", search_long_0)
-        print("lat_1", search_lat_1, "long_1", search_long_1)
-        print("lat_2", search_lat_2, "long_2", search_long_2)
-        print(" ")
-    # home_coordinates
-    # assuming "4" is vehicle: MEDEVAC
-    if y != {}:
-        home_lat = y['2']["lat"]
-        home_long = y['2']["lng"]
-        print("y", y)
-        print("home_lat", home_lat, "home_long", home_long)
-        print(" ")
+#         print("x", x)
+#         print("lat_0", search_lat_0, "long_0", search_long_0)
+#         print("lat_1", search_lat_1, "long_1", search_long_1)
+#         print("lat_2", search_lat_2, "long_2", search_long_2)
+#         print(" ")
+#     # home_coordinates
+#     # assuming "4" is vehicle: MEDEVAC
+#     if y != {}:
+#         home_lat = y['2']["lat"]
+#         home_long = y['2']["lng"]
+#         print("y", y)
+#         print("home_lat", home_lat, "home_long", home_long)
+#         print(" ")
     
-    # drop_coordinates
-    if z != {}:
-        print("z", z)
-        drop_lat = z["lat"]
-        drop_long = z["lng"]
-        print("drop_lat", drop_lat, "drop_long", drop_long)
-        print(" ")
+#     # drop_coordinates
+#     if z != {}:
+#         print("z", z)
+#         drop_lat = z["lat"]
+#         drop_long = z["lng"]
+#         print("drop_lat", drop_lat, "drop_long", drop_long)
+#         print(" ")
 
-    if a != {}:
-        # keep_in: true
-        geo_lat_0_t = a[0]["coordinates"][0]["lat"]
-        geo_lng_0_t = a[0]["coordinates"][0]["lng"]
-        geo_lat_1_t = a[0]["coordinates"][1]["lat"]
-        geo_lng_1_t = a[0]["coordinates"][1]["lng"]
-        geo_lat_2_t = a[0]["coordinates"][2]["lat"]
-        geo_lng_2_t = a[0]["coordinates"][2]["lng"]
+#     if a != {}:
+#         # keep_in: true
+#         geo_lat_0_t = a[0]["coordinates"][0]["lat"]
+#         geo_lng_0_t = a[0]["coordinates"][0]["lng"]
+#         geo_lat_1_t = a[0]["coordinates"][1]["lat"]
+#         geo_lng_1_t = a[0]["coordinates"][1]["lng"]
+#         geo_lat_2_t = a[0]["coordinates"][2]["lat"]
+#         geo_lng_2_t = a[0]["coordinates"][2]["lng"]
 
-        print("a", a)
-        print("lat_0", geo_lat_0_t, "long_0", geo_lng_0_t)
-        print("lat_1", geo_lat_1_t, "long_1", geo_lng_1_t)
-        print("lat_2", geo_lat_2_t, "long_2", geo_lng_2_t)
+#         print("a", a)
+#         print("lat_0", geo_lat_0_t, "long_0", geo_lng_0_t)
+#         print("lat_1", geo_lat_1_t, "long_1", geo_lng_1_t)
+#         print("lat_2", geo_lat_2_t, "long_2", geo_lng_2_t)
 
-        # keep_in: false
-        geo_lat_0_f = a[1]["coordinates"][0]["lat"]
-        geo_lng_0_f = a[1]["coordinates"][0]["lng"]
-        geo_lat_1_f = a[1]["coordinates"][1]["lat"]
-        geo_lng_1_f = a[1]["coordinates"][1]["lng"]
-        geo_lat_2_f = a[1]["coordinates"][2]["lat"]
-        geo_lng_2_f = a[1]["coordinates"][2]["lng"]
+#         # keep_in: false
+#         geo_lat_0_f = a[1]["coordinates"][0]["lat"]
+#         geo_lng_0_f = a[1]["coordinates"][0]["lng"]
+#         geo_lat_1_f = a[1]["coordinates"][1]["lat"]
+#         geo_lng_1_f = a[1]["coordinates"][1]["lng"]
+#         geo_lat_2_f = a[1]["coordinates"][2]["lat"]
+#         geo_lng_2_f = a[1]["coordinates"][2]["lng"]
 
-        print(a[1])
-        print("lat_0", geo_lat_0_f, "long_0", geo_lng_0_f)
-        print("lat_1", geo_lat_1_f, "long_1", geo_lng_1_f)
-        print("lat_2", geo_lat_2_f, "long_2", geo_lng_2_f)
-        print(" ")
+#         print(a[1])
+#         print("lat_0", geo_lat_0_f, "long_0", geo_lng_0_f)
+#         print("lat_1", geo_lat_1_f, "long_1", geo_lng_1_f)
+#         print("lat_2", geo_lat_2_f, "long_2", geo_lng_2_f)
+#         print(" ")
 
 
-def send_cords(ip_address: str, msg_type: str, cords: str):
+def send_cords(ip_address: str, msg_type: str, cords: str, port: int):
     #sends coordinate information to GCS
+    print("inside")
     request_body = {
-        {msg_type}: [
-        {
-            "coordinates": [
+        msg_type: [
             {
-                "lat": cords[0][0],
-                "lng": cords[0][1]
+                "lng": cords[0][0],
+                "lat": cords[0][1]
             },
             {
-                "lat": cords[1][0],
-                "lng":cords[1][1]
+                "lng": cords[1][0],
+                "lat":cords[1][1]
             },
             {
-                "lat": cords[2][0],
-                "lng": cords[2][1]
-            },
-            ],
-        }
+                "lng": cords[2][0],
+                "lat": cords[2][1]
+            }
         ]
     }    
     # POST updated search area coords onto the database. 
     #sends information to GCS 
     try:
-        response = requests.post(f"http://{ip_address}:5000/post{msg_type}/MEA", json=request_body)
+        #requests.post(f"http://{ip_address}:{port}/post{msg_type}", json=request_body)
+        requests.post(f"http://{ip_address}:{port}/{msg_type}", json=request_body)
     except requests.exceptions.JSONDecodeError:
         print("JSONDecodeError: cannot return response body at this time")
     # Cool down period of 0.5 seconds
@@ -273,11 +278,14 @@ def send_cords(ip_address: str, msg_type: str, cords: str):
 
 
 def main():
+    cords = [[25, 10], [15,4], [11, 12]] #test current locations
     ip_address = 'localhost'
-    port = 3000
-    #connect('localhost')
-    client_program(ip_address, port)
-
+    port = 5000
+    # on_message_mod("geo", port)
+    #on_message_mod("search", port)
+    # on_message_mod("evac", port)
+    # on_message_mod("home", port)
+    send_cords(ip_address, "current_location", cords, port)
 if __name__ == '__main__':
     main()
     # while (True):
--- request_server.py
+++ request_server.py
@@ -1,69 +1,53 @@
 import json
-import string
-import time
-import constants
-import requests
-import argparse
-import random
-import socket
-# from flask import Flask, jsonify
-# #send
+from flask import Flask, jsonify
 
+app = Flask(__name__)
 
-# #to do
-# ip_address = 'localhost'
-# port = 3000
-# def post(request_data: requests):
-#     """
-#     Sends status information to GCS
-#     type_msg: type of message sent to GCS
-#     status: quantitative value like coordinates
-#     """
-#     requests.post(f"http://{ip_address}:{port}/get-json", request_data) #stores data inside get-json
-#     time.sleep(0.5)
+@app.route('/geofence', methods=['GET'])
+def getGeofence():
+    with open("db.json") as jsonfile:
+        datavalue = json.load(jsonfile)
+        return json.dumps(datavalue.get("geofence", "")) #sends data in string format
 
-# app = Flask(__name__)
+@app.route('/search_area', methods=['GET'])
+def getSearch():
+    with open("db.json") as jsonfile:
+        datavalue = json.load(jsonfile)
+        return json.dumps(datavalue.get("search_area", ""))
 
+@app.route('/home_coordinates', methods=['GET'])
+def getHome():
+    with open("db.json") as jsonfile:
+        datavalue = json.load(jsonfile)
+        return json.dumps(datavalue.get("home_coordinates", ""))
 
-# @app.route('/get-json', methods=['GET'])
-# def send_cords(data: str):
-#     """
-#     sends coordinate information to get-json
-#     """
-#     search_area = requests.get(f"http://{ip_address}:{port}/search_area")
-#     home_cords = requests.get(f"http://{ip_address}:{port}/home_coordinates")
-#     evac_cords = requests.get(f"http://{ip_address}:{port}/evacuation_coordinates")
-#     geofence = requests.get(f"http://{ip_address}:{port}/geofence")
-#     if data == "search":
-#         post(search_area)
-#     if data == "home":
-#         post(home_cords)
-#     if data == "evac":
-#         post(evac_cords)
-#     if data == "geo":
-#         post(geofence)
-        
-#     # else:
-#     #     send_cords(data) #store data recieved from client onto database
+@app.route('/evacuation_coordinates', methods=['GET'])
+def getEvac():
+    with open("db.json") as jsonfile:
+        datavalue = json.load(jsonfile)
+        return json.dumps(datavalue.get("evacuation_coordinates", ""))
+
+# def connect():
+#     host = socket.gethostname()
+#     port = 3000  # initiate port no above 1024
+#     server_socket = socket.socket()  # get instance
+
+#     server_socket.bind((host, port))  # bind host address and port together
+
+#     server_socket.listen(2)
+#     conn, address = server_socket.accept()  # accept new connection
+    
+#     print("Connection from: " + str(address))
+#     message = input(" -> ")  # take input
+#     while message.lower().strip() != 'bye':
+#         conn.send(message.encode())  # send message
+#         message = input(" -> ")  # again take input
+#     conn.close()  # close the connection
 
 
 
 def main():
-    host = socket.gethostname()
-    port = 3000  # initiate port no above 1024
-    server_socket = socket.socket()  # get instance
-
-    server_socket.bind((host, port))  # bind host address and port together
-
-    server_socket.listen(2)
-    conn, address = server_socket.accept()  # accept new connection
-    
-    print("Connection from: " + str(address))
-    message = input(" -> ")  # take input
-    while message.lower().strip() != 'bye':
-        conn.send(message.encode())  # send message
-        message = input(" -> ")  # again take input
-    conn.close()  # close the connection
+    app.run(debug = True)
 
 if __name__ == '__main__':
     main()
